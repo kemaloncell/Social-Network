3f11bea676862308d24d0d32baa8aefc512f1f46
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-24 00:48:37 +0300

    update and delete user completed

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,46 @@
-const User = require("../models/User")
+const User = require('../models/User');
 const router = require('express').Router();
-
+const bcrypt = require('bcrypt');
 //update user
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        // password update
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body, // outomatic set all inputs inside this body
+      });
+      res.status(200).json('Account has been updated');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You can update only your account!');
+  }
+});
+
 //delete user
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('Account has been deleted');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You can delete only your account!');
+  }
+});
+
 //get a user
 //follow a user
 //unfollow a user
