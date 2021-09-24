946d9d8878daccf69f66b06df15bbabd131633cf
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-24 15:44:40 +0300

    user follower functions completed

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,57 @@ router.delete('/:id', async (req, res) => {
 });
 
 //get a user
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updateAt, ...other } = user._doc; //Password and updateAt not displayed
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //follow a user
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const curretUser = await User.findById(req.body.userId); // karşıdaki kullanıcı
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await curretUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json('user has been followed');
+      } else {
+        res.status(403).json('You allready follow this user');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You cant follow yourself');
+  }
+});
+
 //unfollow a user
+
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const curretUser = await User.findById(req.body.userId); // karşıdaki kullanıcı
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await curretUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json('user has been unfollowed');
+      } else {
+        res.status(403).json('You dont follow this user');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You cant unfollow yourself');
+  }
+});
 
 module.exports = router;
